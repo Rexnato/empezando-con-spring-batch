fa33c76807edcf75ee9dc00c48576356ef5f3466
Renato <renato.noh@atomic32.com>
2023-10-09 18:50:44 -0600

    Tutorial 1 configuracion Spring Batch

---
+++ configuracion-spring-batch/src/test/java/com/jrnoh/demo/empezando/springbatch/ConfiguracionSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrnoh.demo.empezando.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguracionSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
